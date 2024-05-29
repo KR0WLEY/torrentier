090720beda6247a24ae493762f5c5c159ff4ba81
KR0WLEY <50808176+KR0WLEY@users.noreply.github.com>
2024-05-28 21:40:28 -0300

    -- results weren't being show for 1337

--- src/providers/1337x.mjs
+++ src/providers/1337x.mjs
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 async function fetchPage(url) {
     try {
@@ -10,20 +10,12 @@ async function fetchPage(url) {
         return null;
     }
 }
-
 function parseMainPageHTML(html) {
     const $ = cheerio.load(html);
     const torrents = [];
 
     $('.table-list tbody tr').each((index, element) => {
-        const titleElement = $(element).find('.coll-1.name a').last();
-        const title = titleElement.text();
-        const link = titleElement.attr('href');
-        const seeds = $(element).find('.coll-2.seeds').text();
-        const leeches = $(element).find('.coll-3.leeches').text();
-        const uploadDate = $(element).find('.coll-date').text();
-        const size = $(element).find('.coll-4.size').text();
-
+        const titleElement = $(element).find('.coll-1.name a').last();const title = titleElement.text();const link = titleElement.attr('href');const seeds = $(element).find('.coll-2.seeds').text();const leeches = $(element).find('.coll-3.leeches').text();const uploadDate = $(element).find('.coll-date').text();const size = $(element).find('.coll-4.size').text();
         torrents.push({ title, link: `https://www.1377x.to${link}`, seeds, leeches, uploadDate, size });
     });
 
@@ -41,25 +33,46 @@ async function fetchMagnetURL(torrentPageURL) {
     return null;
 }
 
-async function scrape1337x(query, pages = 1) {
-    const baseUrl = 'https://www.1377x.to/search';
+async function scrape1337x(query, categories, pages = 1) {
+    const categoryURL = { Games: `https://www.1377x.to/category-search/${query}/Games/`, Movies: `https://www.1377x.to/category-search/${query}/Movies/`, Animes: `https://www.1377x.to/category-search/${query}/Anime/`, TV: `https://www.1377x.to/category-search/${query}/TV/`, BaseURL: 'https://www.1377x.to/search' };
+
     let allTorrents = [];
 
-    for (let page = 1; page <= pages; page++) {
-        const url = `${baseUrl}/${query}/${page}/`;
-        const html = await fetchPage(url);
+    if (categories.length === 0 || categories.includes('All')) {
+        for (let page = 1; page <= pages; page++) {
+            const url = `${categoryURL.BaseURL}/${query}/${page}/`;
+            const html = await fetchPage(url);
 
-        if (html) {
-            const torrents = parseMainPageHTML(html);
-            for (let torrent of torrents) {
-                const { magnetURL, category } = await fetchMagnetURL(torrent.link);
-                torrent.magnetURL = magnetURL;
-                torrent.category = category;
+            if (html) {
+                const torrents = parseMainPageHTML(html);
+                for (let torrent of torrents) {
+                    const { magnetURL, category } = await fetchMagnetURL(torrent.link);
+                    torrent.magnetURL = magnetURL;
+                    torrent.category = category;
+                }
+                allTorrents = allTorrents.concat(torrents);
             }
-            allTorrents = allTorrents.concat(torrents);
+        }
+    } else {
+        for (let category of categories) {
+            for (let page = 1; page <= pages; page++) {
+                const url = `${categoryURL[category]}/${page}/`;
+                const html = await fetchPage(url);
+
+                if (html) {
+                    const torrents = parseMainPageHTML(html);
+                    for (let torrent of torrents) {
+                        const { magnetURL, category } = await fetchMagnetURL(torrent.link);
+                        torrent.magnetURL = magnetURL;
+                        torrent.category = category;
+                    }
+                    allTorrents = allTorrents.concat(torrents);
+                }
+            }
         }
     }
 
     return allTorrents;
 }
-export { scrape1337x }
+
+export { scrape1337x };
